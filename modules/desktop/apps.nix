ae3fcf9b2c58d6ccf02f52b9dac93aa06ad40451
Nate Morrow <nate@local>
2026-02-22 15:40:04 -0600

    Added Zathura PDF reader to apps.nix.

--- modules/desktop/apps.nix
+++ modules/desktop/apps.nix
@@ -9,4 +9,15 @@
     zed-editor
     inputs.zen-browser.packages."${pkgs.system}".default
   ];
+  programs.zathura = {
+    enable = true;
+    options = {
+      recolor = true;
+      recolor-keephue = true;
+      recolor-darkcolor = "#ebdbb2";
+      recolor-lightcolor = "#282828";
+      default-bg = "#282828";
+      default-fg = "#ebdbb2";
+    };
+  };
 }
