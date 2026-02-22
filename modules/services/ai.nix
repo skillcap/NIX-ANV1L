3f84fa98922c849feb65920bde05fb5a0e3cff8c
Nate Morrow <nate@local>
2026-02-22 14:52:53 -0600

    clearning up ollama nix configuration

--- modules/services/ai.nix
+++ modules/services/ai.nix
@@ -1,11 +1,8 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
-    environmentVariables = {
-      OLLAMA_NUM_CTX = "131072";
-    };
   };
 }
