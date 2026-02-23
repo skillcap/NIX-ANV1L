698efc2341d874dde636b84e9aa6a28f16c45c03
Nate Morrow <nate@local>
2026-02-22 22:05:54 -0600

    Migrating Kitty settings to nix files.

--- modules/desktop/hyprland-user.nix
+++ modules/desktop/hyprland-user.nix
@@ -28,11 +28,61 @@
   };
 
   home.packages = with pkgs; [
-    kitty
     wl-clipboard
     libnotify
     cava
   ];
+
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "CaskaydiaCove Nerd Font";
+      package = pkgs.nerd-fonts.caskaydia-cove;
+      size = 12.0;
+    };
+
+    settings = {
+      window_padding_width = 12;
+      background_opacity = "0.8";
+      hide_window_decorations = "yes";
+      dynamic_background_opacity = "yes";
+
+      cursor_shape = "block";
+      cursor_blink_interval = 1;
+
+      scrollback_lines = 3000;
+
+      copy_on_select = "yes";
+      strip_trailing_spaces = "smart";
+
+      tab_bar_style = "powerline";
+      tab_bar_align = "left";
+
+      shell_integration = "enabled";
+
+      # feels nice, but remove to save power on laptops
+      input_delay = 1;
+      repaint_delay = 5;
+      sync_to_monitor = "yes";
+
+      allow_remote_control = "yes";
+      listen_on = "unix:/tmp/kitty";
+    };
+
+    keybindings = {
+      "ctrl+shift+n" = "new_window";
+      "ctrl+t" = "new_tab";
+      "ctrl+plus" = "change_font_size all +1.0";
+      "ctrl+minus" = "change_font_size all -1.0";
+      "ctrl+0" = "change_font_size all 1";
+    };
+
+    extraConfig = ''
+      include dank-tabs.conf
+      include dank-theme.conf
+    '';
+  };
 
   # Symlinks
   xdg.configFile = {
