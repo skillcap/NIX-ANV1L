0d1fb5e6589320f84281ee8e7b5f41ba543a4015
Nate Morrow <4131420+skillcap@users.noreply.github.com>
2026-02-27 19:47:37 -0600

    Adding games.nix.

--- modules/desktop/games.nix
+++ modules/desktop/games.nix
@@ -4,4 +4,13 @@
   home.packages = [
     inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
   ];
+  xdg.desktopEntries."osu-lazer-pinned" = {
+    name = "osu! (Pinned)";
+    exec = "${pkgs.util-linux}/bin/taskset -c 0-7,16-23 osu! %u";
+    icon = "osu!";
+    terminal = false;
+    type = "Application";
+    categories = [ "Game" ];
+    comment = "osu! pinned to 3D V-Cache CCD";
+  };
 }
