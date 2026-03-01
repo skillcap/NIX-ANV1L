416208a962953699677ce60b6e2d67296904f8f9
Nate Morrow <4131420+skillcap@users.noreply.github.com>
2026-03-01 15:52:16 -0600

    Updating Steam Game configuration. Removing Gamescope, moving extraEnvs to Steam, simplifying gamescripts.

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -5,8 +5,10 @@
     enable = true;
     settings = {
       general = {
-        softrealtime = "auto";
         renice = 9;
+      };
+      cpu = {
+        pin_cores = "0-7,16-23";
       };
     };
   };
@@ -16,7 +18,18 @@
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = false;
     platformOptimizations.enable = true;
-    gamescopeSession.enable = true;
+    gamescopeSession.enable = false;
+    package = pkgs.steam.override {
+      extraEnv = {
+        SDL_VIDEODRIVER = "wayland";
+        SDL_VIDEO_DRIVER = "wayland";
+        WINE_CPU_TOPOLOGY = "16:0,1,2,3,4,5,6,7,16,17,18,19,20,21,22,23";
+        VKD3D_CONFIG = "dxr,dxr11";
+
+        # Moved back here to eliminate redundancy in the scripts
+        MANGOHUD_CONFIG = "position=top-right,toggle_hud=bracketright,fps,frametime,cpu_temp,gpu_temp,vram,ram,fps_limit=173,no_display";
+      };
+    };
   };
 
   # --- Gaming Cache ---
@@ -25,55 +38,20 @@
     trusted-public-keys = [ "nix-gaming.cachix.org-3:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
   };
 
-  environment.sessionVariables = {
-    DXVK_HDR = "1";
-    ENABLE_GAMESCOPE_WSI = "1";
-  };
-
-  # Configure for your primary display
-  # Add to steam game launch options
-  # gs %command%
-  # gs2 grabs cursor for games that need it. ~CS2
   environment.systemPackages = with pkgs; [
     mangohud
-    (pkgs.writeShellScriptBin "gs" ''
-        export VCACHE_CORES="0,1,2,3,4,5,6,7,16,17,18,19,20,21,22,23"
-        export WINE_CPU_TOPOLOGY="16:$VCACHE_CORES"
-        export SDL_VIDEODRIVER=wayland
-        export SDL_VIDEO_DRIVER=wayland
-        export MANGOHUD_CONFIG="fps_limit=173,no_display"
+    (writeShellScriptBin "gs" ''
+      export DXVK_HDR=1
+      export PROTON_ENABLE_HDR=1
+      export PROTON_ENABLE_WAYLAND=1
+      export ENABLE_HDR_WSI=1
+      export VKD3D_CONFIG="hdr,dxr,dxr11"
 
-        # Proton and DXVK HDR flags
-        export ENABLE_GAMESCOPE_WSI=1
-        export DXVK_HDR=1
-        export PROTON_ENABLE_HDR=1
-
-        exec gamescope \
-        -O DP-3 -W 3440 -H 1440 -r 175 -f \
-        --adaptive-sync --hdr-enabled \
-        --hdr-sdr-content-nits 1000 \
-        -- \
-        gamemoderun mangohud taskset -ac $VCACHE_CORES "$@"
+      exec gamemoderun mangohud "$@"
     '')
-    (pkgs.writeShellScriptBin "gs2" ''
-        export VCACHE_CORES="0,1,2,3,4,5,6,7,16,17,18,19,20,21,22,23"
-        export WINE_CPU_TOPOLOGY="16:$VCACHE_CORES"
-        export SDL_VIDEODRIVER=wayland
-        export SDL_VIDEO_DRIVER=wayland
-        export MANGOHUD_CONFIG="fps_limit=173,no_display"
-
-        # Proton and DXVK HDR flags
-        export ENABLE_GAMESCOPE_WSI=1
-        export DXVK_HDR=1
-        export PROTON_ENABLE_HDR=1
-
-        exec gamescope \
-          -O DP-3 -W 3440 -H 1440 -r 175 -f \
-          --adaptive-sync --hdr-enabled \
-          --force-grab-cursor \
-          --hdr-sdr-content-nits 1000 \
-          -- \
-          gamemoderun mangohud taskset -ac $VCACHE_CORES "$@"
+    # For no HDR
+    (writeShellScriptBin "gs2" ''
+      exec gamemoderun mangohud "$@"
     '')
   ];
 }
